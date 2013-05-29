188eb9ad3950cad38595b21d288ee35a25fb6455
Ryan Chew <kczao81@gmail.com>
2013-05-29 20:08:53 +0800

    First commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'pg', '0.14.0'
+gem 'jquery-rails', '2.2.1'
+
+#app specific gems
+gem 'devise', '2.2.4'
+#
+
+group :assets do
+  gem 'sass-rails',   '3.2.5'
+  gem 'coffee-rails', '3.2.2'
+  gem 'haml', '4.0.2'
+  gem 'uglifier', '1.2.7'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoppie::Application.config.secret_token = 'e574716692f783d1ac827a2090e03bdcc7712f147feaecc81eb81018152b79e04a4555821e694e4ae425c88ef064e7ac3150acfadf252f5c78f978464248c75f'
